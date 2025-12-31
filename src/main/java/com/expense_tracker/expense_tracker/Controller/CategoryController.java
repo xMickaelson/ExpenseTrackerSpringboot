ee6fc364b19f7f6b23035bf41cca239f3e5800f6
Satyam Singh <satyamsingh@Satyams-MacBook-Air.local>
2025-12-31 18:58:39 +0530

    added swagger dependency

--- src/main/java/com/expense_tracker/expense_tracker/Controller/CategoryController.java
+++ src/main/java/com/expense_tracker/expense_tracker/Controller/CategoryController.java
@@ -2,6 +2,7 @@ package com.expense_tracker.expense_tracker.Controller;
 
 import com.expense_tracker.expense_tracker.Entity.Category;
 import com.expense_tracker.expense_tracker.Service.Category.CategoryService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +14,7 @@ public class CategoryController {
     private CategoryService categoryService;
 
     @PostMapping("/api/v1/createCategory")
+    @Operation(summary = "Add New Category")
     public Category createCategory(@RequestBody Category category){
         return categoryService.createCategory(category);
     }
--- src/main/java/com/expense_tracker/expense_tracker/Controller/ExpenseController.java
+++ src/main/java/com/expense_tracker/expense_tracker/Controller/ExpenseController.java
@@ -2,6 +2,7 @@ package com.expense_tracker.expense_tracker.Controller;
 
 import com.expense_tracker.expense_tracker.Entity.Expense;
 import com.expense_tracker.expense_tracker.Service.Expenses.ExpenseService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
@@ -15,27 +16,32 @@ public class ExpenseController {
     private ExpenseService expenseService;
 
     @PostMapping("/api/v1/addExpense")
+    @Operation(summary = "Add New Expense")
     public Expense addExpense(@RequestBody Expense payload) {
         return expenseService.addExpense(payload);
     }
 
     @GetMapping("/api/v1/getAllExpense")
+    @Operation(summary = "List All Expenses")
     public List<Expense> getAllExpenses(){
         List<Expense> allExpensesList = expenseService.getAllExpenses();
         return allExpensesList;
     }
 
     @GetMapping("/api/v1/getExpense")
+    @Operation(summary = "Get Expense By Category")
     public List<Expense> getExpensesByCategory(@RequestParam String categoryName){
         return expenseService.getExpensesByCategory(categoryName);
     }
 
     @GetMapping("/api/v1/summary")
+    @Operation(summary = "Summarize expense by month and year")
     public List<Object []> getMonthlySalary(@RequestParam int year, @RequestParam int month){
         return expenseService.getMonthlyExpenseSummary(year, month);
     }
 
     @GetMapping("/api/v1/expenses")
+    @Operation(summary = "Expense Paging and Sorting")
     public Page<Expense> getAllExpensesPage(
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "10") int size,
